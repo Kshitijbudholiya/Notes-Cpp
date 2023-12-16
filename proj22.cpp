99253adc74e8b6788c3704072f8348cc026b80b9
Kshitij Budholiya <kshitijbudholiya2006@gmail.com>
2023-12-16 10:40:37 +0530

    Polymorpism Example Added

--- proj22.cpp
+++ proj22.cpp
@@ -1,110 +1,32 @@
 #include<iostream>
 using namespace std;
 
-class ClassOne
+/*
+This is called as Methord Overiding.
+It is a part of Polymorpism.
+*/
+
+class ParentOne
 {
     public:
-    int numClassOne = 1;
-    string stringClassOne = "One";
-    
-    void defClassOne()
+    void show()
     {
-        cout<<"Variables in ClassOne are: "<<numClassOne<<" & "<<stringClassOne<<endl;
-        cout<<"It is a default function of ClassOne"<<endl;
-    }
-
-    void paraClassOne(int paraOne, int paraTwo)
-    {
-        cout<<"It is a parametrized function in ClassOne which stores: "<<paraOne<<" & "<<paraTwo<<endl<<endl;
-    }
-
-    ClassOne(int paraOne, int paraTwo)
-    {
-        cout<<"It is a parametrized constructor in ClassOne which stores: "<<paraOne<<" & "<<paraTwo<<endl;
-    }
-
-    ClassOne()
-    {
-        cout<<"Default Constructor of ClassOne"<<endl;
+        cout<<"Parent";
     }
 };
 
-class ClassTwo
+class Child : public ParentOne
 {
     public:
-    int numClassTwo = 2;
-    string stringClassTwo = "Two";
-    
-    void defClassTwo()
+    void show()
     {
-        cout<<"It is a default function of ClassTwo"<<endl;
-        cout<<"Variables in ClassTwo are: "<<numClassTwo<<" & "<<stringClassTwo<<endl;
+        cout<<"Child";
     }
-
-    void paraClassTwo(int paraOne, int paraTwo)
-    {
-        cout<<"It is a parametrized function in ClassTwo which stores: "<<paraOne<<" & "<<paraTwo<<endl<<endl;
-    }
-
-    ClassTwo(int paraOne, int paraTwo)
-    {
-        cout<<"It is a parametrized constructor in ClassTwo which stores: "<<paraOne<<" & "<<paraTwo<<endl;
-    }
-
-    ClassTwo()
-    {
-        cout<<"Default Constructor of ClassTwo"<<endl;
-    }
-
-    ~ClassTwo(){};
-};
-
-class ClassThree:public ClassOne, public ClassTwo
-{
-    public:
-    int numClassThree = 3;
-    string stringClassThree = "Three";
-    
-    void defClassThree()
-    {
-        cout<<"Variables in ClassThree are: "<<numClassThree<<" & "<<stringClassThree<<endl;
-        cout<<"It is a default function of ClassThree"<<endl;
-    }
-
-    void paraClassThree(int paraOne, int paraTwo)
-    {
-        cout<<"It is a parametrized function in ClassThree which stores: "<<paraOne<<" & "<<paraTwo<<endl<<endl;
-    }
-    
-    ClassThree(int paraOne, int paraTwo)
-    {
-        cout<<"It is a parametrized constructor in ClassThree which stores: "<<paraOne<<" & "<<paraTwo<<endl;
-    }
-
-    ClassThree()
-    {
-        cout<<"Default Constructor of ClassThree"<<endl;
-
-        // Class Three
-        ClassThree(12,13);
-        defClassThree();
-        paraClassThree(120, 24);
-        
-        // Class Two
-        ClassTwo(45,47);
-        defClassTwo();
-        paraClassTwo(98, 54);
-
-        // Class One
-        ClassOne(39, 72);
-        defClassOne();
-        paraClassOne(29, 55);
-    }
-
 };
 
 int main()
 {
-    ClassThree obj = ClassThree();
+    Child obj = Child();
+    obj.show();
     return 0;
 }
